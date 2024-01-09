9d2d8a9f0ed09b03ad1adadca4b72c71f3b34927
Guuzzeji <5lh8dl440f@gmail.com>
2024-01-09 14:02:19 -0800

    Testing out more strucutre in prompt to get better results from GPT, respones are still tje same however, the only that has change is getting respones from harder images

--- openaiService/src/prompts/videoAnalyze.js
+++ openaiService/src/prompts/videoAnalyze.js
@@ -4,19 +4,23 @@ import { PromptTemplate } from "langchain/prompts";
 
 export const VideoAnalysisPrompt = new PromptTemplate({
     inputVariables: ["audio_transcription", "title"],
-    template: `As a professional image analyzer, your task is to meticulously generate a visual description based on screenshots taken from a video using the following steps:
+    template: `As a professional Media Content Analyst, your task is to meticulously generate a visual description based on screenshots taken from a video using the following steps:
 
 Step 1) Context Establishment: Initiate by comprehending the context. Extract significant details from the provided video title: "{title}". Utilize this information as a guiding framework for your visual description. Additionally, if available, extract insights from the audio transcription: "{audio_transcription}".
     
 Step 2) Image Sequence Analysis: Scrutinize a sequence of images derived from a video. Acknowledge the interconnection and sequential dependence of these screenshots.
     
-Step 3) Focus on Key Aspects: Direct your attention towards overarching concepts, ideas, charts, text (including code), and other essential elements found collectively within the images. Provide a visual description that encapsulates the overall content without detailing each individual screenshot.
+Step 3) Focus on Key Aspects: Direct your attention towards overarching actions, charts, diagrams, text (including code), and other essential elements found collectively within the images. Provide a visual description that encapsulates the overall content without detailing each individual screenshot.
     
 Step 4) Assumption in Ambiguity: In the event of unclear images, draw upon information from other screenshots, the video title, and the audio transcription to make informed assumptions.
     
-Step 5) General Description: Craft a concise yet highly detailed visual description of the collective content of all screenshots. Integrate the context you established to enrich the depiction. Also (MOST IMPORTANT), transcribe any text, diagrams, or code found within the images. Limit the visual description to BETWEEN 1 and 2 sentences, with a LIMIT of 250 words total. Exclude the video title from your response.
+Step 5) Visual Description: Craft a detailed yet concise depiction of the collective content in the screenshots. Integrate established context for richness. Emphasize transcribing text or code as a priority; provide equally detailed descriptions of diagrams or charts. Keep the visual description within 1 to 2 sentences, limiting the total words to 75-250. Exclude the video title. 
     
-Remember your role as a professional image analyzer, utilizing a structured chain of reasoning to synthesize information and deliver an insightful general description. In the event you cannot generate a response, state: "I cannot generate anything with the information given". YOU CAN DO IT AND TRY YOUR BEST!!`
+Remember your role as a professional Media Content Analyst, your task is to meticulously generate a visual description based on screenshots taken from a video. In the event you cannot generate a response, state: “I cannot generate anything with the information given.” 
+
+Remember to always try to create a visual description no matter what!!
+
+Screenshots From Video As One Single Image:`
 });
 
 export function secondsToTimeCode(seconds) {
