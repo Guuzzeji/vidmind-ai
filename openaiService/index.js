619dcd7f3e193041de7cb1fec5cc8ca94293e8a2
Guuzzeji <5lh8dl440f@gmail.com>
2024-01-09 18:03:00 -0800

    Simple test code to see if everything is working

--- openaiService/index.js
+++ openaiService/index.js
@@ -5,14 +5,18 @@ import 'dotenv/config';
 // ! IMPORTANT FIX
 // TODO: Create a token algorthiem that can sort sort the array of prompts and set of langchain to do the correct amount of prompt process
 
-const TEST_VID_ID = "SmyPTnlqhlk";
+const TEST_VID_ID = "za3FN2fvXa8";
 
-let videoBreakDown = new VideoTranscript(TEST_VID_ID);
-videoBreakDown.initialize();
+const TEST_VIDEO = new VideoTranscript(TEST_VID_ID);
+await TEST_VIDEO.initialize();
 
-for (let text of videoBreakDown.visualTranscript) {
-    console.log(text);
-}
+console.log(TEST_VIDEO.visualTranscript);
+
+await TEST_VIDEO.createEmbedVisualTranscript();
+await TEST_VIDEO.createEmbedAudioTranscript();
+
+console.log(TEST_VIDEO.embedAudioTranscript);
+console.log(TEST_VIDEO.embedVisualTranscript)
 
 
 
