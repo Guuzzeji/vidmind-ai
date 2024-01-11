a3756ee2d08b5411c765763b6a67f88ba90c64e1
Guuzzeji <5lh8dl440f@gmail.com>
2024-01-10 21:30:07 -0800

    Getting sorting of embedings base on search term

--- openaiService/index.js
+++ openaiService/index.js
@@ -5,7 +5,7 @@ import 'dotenv/config';
 // ! IMPORTANT FIX
 // TODO: Create a token algorthiem that can sort sort the array of prompts and set of langchain to do the correct amount of prompt process
 
-const TEST_VID_ID = "za3FN2fvXa8";
+const TEST_VID_ID = "MtmWI2smwgY";
 
 const TEST_VIDEO = new VideoTranscript(TEST_VID_ID);
 await TEST_VIDEO.initialize();
@@ -16,8 +16,11 @@ await TEST_VIDEO.createEmbedVisualTranscript();
 await TEST_VIDEO.createEmbedAudioTranscript();
 
 console.log(TEST_VIDEO.embedAudioTranscript);
-console.log(TEST_VIDEO.embedVisualTranscript)
+console.log(TEST_VIDEO.embedVisualTranscript);
 
+let sortedBySearch = await TEST_VIDEO.searchVisualTranscript("What is star:52?");
+
+console.log(sortedBySearch[0])
 
 
 
