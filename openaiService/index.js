ed31531a8a95c990d1f82bb0795d381e3d43ceca
Gabe <46883697+Guuzzeji@users.noreply.github.com>
2024-01-11 19:53:47 -0800

    Embeding audio and visual (Rewrote as class base) (#4)
    * Adding embeding function to project.
    
    * Switch to Class system for breaking down video, doing this because we can use infomation, rather then having to recall openai, which will cost more.
    
    * Fixing typo in array loop
    
    * Fixing general errors within class file to get everything working.
    
    * Simple test code to see if everything is working
    
    * Adding embed search, using compute-cosine-similarity libary to do search
    
    * Adding more about how different companies are doing the same thing
    
    * Fixing error in embed calls to openai, forget to repace calls with new var.
    
    * Getting sorting of embedings base on search term

--- openaiService/index.js
+++ openaiService/index.js
@@ -1,28 +1,27 @@
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { createPromptsForVideo } from "./src/createVisualDescription.js";
+import { VideoTranscript } from "./src/VideoTranscript.js";
 import 'dotenv/config';
 
 // ! IMPORTANT FIX
 // TODO: Create a token algorthiem that can sort sort the array of prompts and set of langchain to do the correct amount of prompt process
 
-const TEST_VID_ID = "SmyPTnlqhlk";
+const TEST_VID_ID = "MtmWI2smwgY";
 
-let prompts = await createPromptsForVideo(TEST_VID_ID);
+const TEST_VIDEO = new VideoTranscript(TEST_VID_ID);
+await TEST_VIDEO.initialize();
 
-const chat = new ChatOpenAI({
-    openAIApiKey: process.env.OPENAI_API_KEY,
-    modelName: "gpt-4-vision-preview",
-    temperature: 0.05,
-    maxTokens: 250,
-    maxConcurrency: 3,
-    streaming: false,
-});
+console.log(TEST_VIDEO.visualTranscript);
 
-let respones = await chat.batch(prompts);
+await TEST_VIDEO.createEmbedVisualTranscript();
+await TEST_VIDEO.createEmbedAudioTranscript();
 
-for (let messages of respones) {
-    console.log(messages.content + '\n');
-}
+console.log(TEST_VIDEO.embedAudioTranscript);
+console.log(TEST_VIDEO.embedVisualTranscript);
+
+let sortedBySearch = await TEST_VIDEO.searchVisualTranscript("What is star:52?");
+
+console.log(sortedBySearch[0])
+
 
 
 
