9158a22c5c0643befe23201b1ba7accfcf34028b
Guuzzeji <5lh8dl440f@gmail.com>
2024-01-27 16:31:19 -0800

    Editing chat prompt to give better respones to user

--- openaiService/chat.test.js
+++ openaiService/chat.test.js
@@ -12,7 +12,7 @@ import { ClipSearchTool } from './src/functionCalling/answerImageQuestion.js';
 const OPENAI_CALL = new ChatOpenAI({
     openAIApiKey: process.env.OPENAI_API_KEY,
     modelName: "gpt-3.5-turbo-1106",
-    temperature: 0.5,
+    temperature: 0.0,
     // maxTokens: 250,
     maxConcurrency: 1,
     streaming: true,
@@ -38,6 +38,8 @@ const agentExecutor = new AgentExecutor({
     verbose: true,
     returnIntermediateSteps: true,
     maxIterations: 10,
+    handleParsingErrors: true // used to stop error exit
+
 });
 
 prompt.start();
@@ -58,23 +60,27 @@ while (true) {
     let sortedAudioEmbed = await videoMetaData.searchAudioTranscript(user.question);
 
     let msg = await agentExecutor.invoke({
-        input: `As a professional media content analyzer, your task is to answer user questions based on the provided information about the video. Your response should utilize both the Audio Transcript and Visual Description given, along with the infomation given to you by tools.
+        input: `As a professional media content analyzer, your task is to answer user questions based on the provided information about the video. Your response should utilize both the Audio Transcript and Visual Description given, along with the information obtained in the following steps:
 
-Your primary goal is to address the user's question effectively.
-        
-Craft your responses with precision and clarity, focusing on delivering accurate and relevant information to meet the user's inquiry. You should always write your responses in Markdown.
-        
-If you cannot come up with a solution to the question, please write "Sorry cannot answer this question."
+Step 1) Extracting Key Information and Main Content: Before delving into the video details extract key information from the user's question, focusing on the main content the user is seeking. Identify details and key information that can help find the correct answer to the user's question.
+
+Step 2) Focusing on Relevant Details for User Understanding: Once the user's question is understood, concentrate on details that are directly relevant to the user's inquiry. Provide specific information to enhance the user's understanding and address the main content the user is seeking use Audio Transcript and Visual Description given to you.
+
+Step 3) Crafting Responses with Precision and Context: Before finalizing responses, carefully review and ensure that the crafted answers directly address the user's questions. Confirm that the responses align with the main content and key details identified in Step 1. Emphasize precision and context alignment in the answers.
+
+Step 4) Checking Responses Against the Original Question: After generating responses, compare them to the original user question. Confirm that the responses effectively address the main content and key details identified in Step 1. Use this step as a final check to ensure that the generated answers accurately reflect the user's intent and provide valuable information.
+
+Goal: Address the user's question effectively. Use Markdown for your responses.
 
 Video Information:
-    Video ID: "${video.id}"
-    Visual Description:
-        Clip ${sortedVisualEmbed[0].id}: "${sortedVisualEmbed[0].text}"
-        Clip ${sortedVisualEmbed[1].id}: "${sortedVisualEmbed[1].text}"
-        Clip ${sortedVisualEmbed[2].id}: "${sortedVisualEmbed[2].text}"
-        Audio Transcript: "${sortedAudioEmbed[0].text + ' ' + sortedAudioEmbed[1].text + ' ' + sortedAudioEmbed[2].text}"
+    - Video ID: "${video.id}"
+    - Visual Description:
+        - Clip ${sortedVisualEmbed[0].id}: "${sortedVisualEmbed[0].text}"
+        - Clip ${sortedVisualEmbed[1].id}: "${sortedVisualEmbed[1].text}"
+        - Clip ${sortedVisualEmbed[2].id}: "${sortedVisualEmbed[2].text}"
+    - Audio Transcript: "${sortedAudioEmbed[0].text + ' ' + sortedAudioEmbed[1].text + ' ' + sortedAudioEmbed[2].text}"
 
-User Question: ${user.question}`
+User Question: "${user.question}`
     });
 
     console.log("--START Embed Infomation Used ---");
@@ -86,7 +92,7 @@ User Question: ${user.question}`
     console.log(`${sortedAudioEmbed[0].text + '\n\n' + sortedAudioEmbed[1].text + '\n\n' + sortedAudioEmbed[2].text}`);
     console.log("--END Embed Infomation Used ---");
 
-    console.log("--- START of GPT message ---");
+    console.log("\n--- START of GPT message ---");
     console.log(msg.output);
     console.log("--- END of GPT message ---\n");
 }
