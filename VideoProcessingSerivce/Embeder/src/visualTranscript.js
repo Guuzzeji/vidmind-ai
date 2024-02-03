53c2c4d81471f82c45cd87c178e105d10bfb40ea
Guuzzeji <5lh8dl440f@gmail.com>
2024-02-03 12:55:07 -0800

    Editing visual transcript to be func base and use txt to store prompt.

---
+++ VideoProcessingSerivce/Embeder/src/visualTranscript.js
@@ -0,0 +1,58 @@
+import { PromptTemplate } from "@langchain/core/prompts";
+import { HumanMessage } from "@langchain/core/messages";
+import { ChatOpenAI } from "@langchain/openai";
+
+import { getPromptText } from "./utils.js";
+import { trimTextByTokenAmount } from './audioTranscript.js';
+
+const PATH_TO_PROMPT = "./src/prompts/visual_transcript_prompt.txt";
+const VideoAnalysisPrompt = new PromptTemplate({
+    inputVariables: ["audio_transcription", "title"],
+    template: getPromptText(PATH_TO_PROMPT)
+});
+
+const OPENAI_CALL = new ChatOpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    modelName: "gpt-4-vision-preview",
+    temperature: 0.05,
+    maxTokens: 250,
+    maxConcurrency: 3,
+    streaming: false,
+});
+
+export async function createVisualPrompt(title, audio_transcription, img) {
+    let trimText = trimTextByTokenAmount(audio_transcription, 325);
+
+    let prompt = await VideoAnalysisPrompt.format({
+        title: title,
+        audio_transcription: trimText,
+    });
+
+    // for some reason you have to set prompt format like this for langchain to 
+    // work right
+    return [
+        new HumanMessage({
+            content: [
+                { "type": "text", "text": prompt },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "data:image/jpeg;base64," + img,
+                        "detail": "low",
+                    }
+                }
+            ]
+        })
+    ];
+}
+
+export async function batchCallOpenAI(prompts) {
+    let respones = await OPENAI_CALL.batch(prompts);
+
+    let cleanTexts = [];
+    for (let x = 0; x < respones.length; x++) {
+        cleanTexts.push(respones[x].content);
+    }
+
+    return cleanTexts;
+}
