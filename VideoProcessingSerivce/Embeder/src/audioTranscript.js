8002412eb8b8a47f0aca32b86e47cafff6fa51bf
Guuzzeji <5lh8dl440f@gmail.com>
2024-02-03 12:55:59 -0800

    Making whisper take base64 files and removing chunking text stuff because we are chunking text when chopping up the video

--- VideoProcessingSerivce/Embeder/src/audioTranscript.js
+++ VideoProcessingSerivce/Embeder/src/audioTranscript.js
@@ -1,5 +1,4 @@
-import fs from 'fs';
-import OpenAI from 'openai';
+import OpenAI, { toFile } from 'openai';
 import { encode } from 'gpt-tokenizer';
 import 'dotenv/config';
 
@@ -7,75 +6,23 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
 
-export async function createTranscript(file) {
+export async function createTranscript(fileBase64) {
     const transcription = await openai.audio.transcriptions.create({
-        file: fs.createReadStream(file),
+        file: await toFile(Buffer.from(fileBase64, 'base64'), "audio.mp3", {
+            contentType: "audio/mp3"
+        }),
         model: "whisper-1",
-        response_format: 'verbose_json'
+        temperature: 0.2,
+        response_format: 'text'
     });
 
-    //console.log(transcription);
-
-    let cleanTranscription = [];
-    for (let x = 0; x < transcription.segments.length; x++) {
-        cleanTranscription.push({
-            id: transcription.segments[x].id,
-            start: transcription.segments[x].start,
-            end: transcription.segments[x].end,
-            text: transcription.segments[x].text
-        });
-    }
-
-    return cleanTranscription;
-}
-
-export function findTranscriptionWithBounds(transcription, start, end) {
-    let startIndex = searchByBound(transcription, start, "start", 0, transcription.length - 1);
-    let endIndex = searchByBound(transcription, end, "end", startIndex, transcription.length - 1);
-
-    // console.log(startIndex, endIndex);
-
-    let boundScripts = transcription.slice(startIndex, startIndex + endIndex);
-    let textScript = "";
-
-    if (boundScripts.length == 0) {
-        return "None";
-    }
-
-    // DO NOT USE JOIN, it will not work b/c we are using objects in array
-    for (let x = 0; x < boundScripts.length; x++) {
-        textScript += boundScripts[x].text;
-    }
-
-    return textScript.trim();
-}
-
-function searchByBound(transcription, time, timeBound, low, high) {
-    let middle = 0;
-    while (low <= high) {
-        middle = Math.floor(low + (high - low) / 2);
-
-        //console.log(middle);
-
-        if (transcription[middle][timeBound] == time) {
-            return middle;
-        }
-
-        if (transcription[middle][timeBound] < time) {
-            low = middle + 1;
-        } else {
-            high = middle - 1;
-        }
-    }
-
-    return middle;
+    return transcription;
 }
 
 export function trimTextByTokenAmount(text, maxTokens) {
     let editText = text;
     while (encode(editText).length >= maxTokens) {
         editText = editText.slice(0, -1);
-        //console.log(encode(editText));
     }
 
     return editText;
