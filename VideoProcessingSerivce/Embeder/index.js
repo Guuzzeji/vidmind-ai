eaaaa689578d59126e2b7b735f74ba85514ca170
Guuzzeji <5lh8dl440f@gmail.com>
2024-02-04 21:29:09 -0800

    Creating redis reader to get tickets from stream process that package with openai

--- VideoProcessingSerivce/Embeder/index.js
+++ VideoProcessingSerivce/Embeder/index.js
@@ -1,27 +1,87 @@
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import { VideoTranscript } from "./src/VideoTranscript.js";
-import 'dotenv/config';
+import { createClient } from 'redis';
 
-// ! IMPORTANT FIX
-// TODO: Create a token algorthiem that can sort sort the array of prompts and set of langchain to do the correct amount of prompt process
+import { insertVideoMetadata, insertS3Urls, insertVisualEmbed, insertAudioEmbed } from './src/db.js';
 
-const TEST_VID_ID = "MtmWI2smwgY";
+import { createTranscript, trimTextByTokenAmount } from './src/audioTranscript.js';
+import { createVisualPrompt, batchCallOpenAI } from './src/visualTranscript.js';
+import { embedTextList } from './src/embed.js';
 
-const TEST_VIDEO = new VideoTranscript(TEST_VID_ID);
-await TEST_VIDEO.initialize();
+import { getBase64 } from './src/utils.js';
+import { setTimeout } from 'timers/promises';
 
-console.log(TEST_VIDEO.visualTranscript);
-
-await TEST_VIDEO.createEmbedVisualTranscript();
-await TEST_VIDEO.createEmbedAudioTranscript();
-
-console.log(TEST_VIDEO.embedAudioTranscript);
-console.log(TEST_VIDEO.embedVisualTranscript);
-
-let sortedBySearch = await TEST_VIDEO.searchVisualTranscript("What is star:52?");
-
-console.log(sortedBySearch[0])
+const clientRedis = await createClient({
+    url: process.env.REDIS_URL
+}).connect();
 
 
+while (true) {
+    console.log("Checking...");
+    let rawResult = await clientRedis.sendCommand(["XREADGROUP", "GROUP", "embed_worker", "worker0", "STREAMS", "EMBED_TICKETS", ">"]);
+    // console.log(rawResult);
 
+    if (rawResult != null && rawResult[0][1].length != 0) {
+        let redisPackage = JSON.parse(rawResult[0][1][0][1][1]);
+        console.log(redisPackage);
 
+        let visualPrompts = [];
+        let audioTexts = [];
+
+        await insertVideoMetadata({ id: redisPackage.id, title: redisPackage.title, numOfClips: redisPackage.chunks.length });
+
+        // Adding and preprocess stuff for video
+        for (let i = 0; i < redisPackage.chunks.length; i++) {
+            let chunk = redisPackage.chunks[i];
+
+            // Adding files to db
+            await insertS3Urls({ id: redisPackage.id, clipId: chunk.id, audioUrl: chunk.audio_url, frameUrl: chunk.frame_url });
+
+            // Preprocessing audio
+            let audioFile = await getBase64(chunk.audio_url);
+            let audioText = await createTranscript(audioFile);
+            audioTexts.push(audioText);
+
+            // Creating visual prompts
+            let img = await getBase64(chunk.frame_url);
+            let visualPrompt = await createVisualPrompt(redisPackage.title, trimTextByTokenAmount(audioText, 250), img);
+            visualPrompts.push(visualPrompt);
+        }
+
+        // Having visual elements process by gpt and embed
+        let resVisualPrompt = await batchCallOpenAI(visualPrompts);
+        let audioEmbed = await embedTextList(audioTexts);
+        let visualEmbed = await embedTextList(resVisualPrompt);
+
+        // Adding embed to db
+        for (let i = 0; i < redisPackage.chunks.length; i++) {
+            let startTime = redisPackage.chunks[i].startTime;
+            let endTime = redisPackage.chunks[i].endTime;
+
+            await insertVisualEmbed({
+                id: redisPackage.id,
+                clipId: i,
+                embedding: "[" + String(visualEmbed[i].embed) + "]",
+                rawText: visualEmbed[i].text,
+                start: startTime,
+                end: endTime
+            });
+
+            await insertAudioEmbed({
+                id: redisPackage.id,
+                clipId: i,
+                embedding: "[" + String(audioEmbed[i].embed) + "]",
+                rawText: audioEmbed[i].text,
+                start: startTime,
+                end: endTime
+            });
+        }
+
+        // Remove ticket from queue
+        let redisId = rawResult[0][1][0][0];
+        await clientRedis.sendCommand(["XACK", "EMBED_TICKETS", "embed_worker", redisId]);
+    } else {
+        // TODO: Check for pending tickets for nothing new was added to stream
+        console.log("nothing to do...");
+    }
+
+    await setTimeout(5 * 1000); // Sleep before next check up
+}
