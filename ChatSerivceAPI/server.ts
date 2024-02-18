e4a8dad044205040a1eef26899405ab97da6e95d
Guuzzeji <5lh8dl440f@gmail.com>
2024-02-17 17:59:06 -0800

    Adding notes on where got infomation about setting up trpc with express

--- ChatSerivceAPI/server.ts
+++ ChatSerivceAPI/server.ts
@@ -1,6 +1,13 @@
 import * as trpcExpress from '@trpc/server/adapters/express';
 import express from 'express';
 
+/**
+ * Notes
+ * - https://trpc.io/docs/server/adapters/express
+ * - https://trpc.io/docs/quickstart
+ * - https://medium.com/@Aryan_Gupta/node-js-express-typescript-eslint-boilerplate-setup-5ae761a0eff1
+ */
+
 import { appRouter, createContext } from './src/router/api.ts';
 
 const app = express();
