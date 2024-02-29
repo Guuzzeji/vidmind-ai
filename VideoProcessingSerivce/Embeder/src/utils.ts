6437de044781fb3d12034bcb3ea96e4faca4605d
Guuzzeji <5lh8dl440f@gmail.com>
2024-02-29 09:17:18 -0800

    Adding time stamp func

--- VideoProcessingSerivce/Embeder/src/utils.ts
+++ VideoProcessingSerivce/Embeder/src/utils.ts
@@ -7,3 +7,14 @@ export async function getBase64(url: string): Promise<string> {
         })
         .then(response => Buffer.from(response.data, 'binary').toString('base64'));
 }
+
+export function secondsToTimestamp(convertSeconds: number): string {
+    let dateObj = new Date(convertSeconds * 1000);
+    let hours = dateObj.getUTCHours();
+    let minutes = dateObj.getUTCMinutes();
+    let seconds = dateObj.getSeconds();
+
+    return hours.toString().padStart(2, '0')
+        + ':' + minutes.toString().padStart(2, '0')
+        + ':' + seconds.toString().padStart(2, '0');
+}
