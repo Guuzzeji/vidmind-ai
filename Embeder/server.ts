6fd6df3fc4f89645938eb1abd35ba5c58b4ad240
Guuzzeji <5lh8dl440f@gmail.com>
2024-05-04 11:13:19 -0700

    Adding logger for embeder

--- Embeder/server.ts
+++ Embeder/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-import { API_ROUTER } from './src/api';
+import morgan from 'morgan';
 import 'dotenv/config'
 
+import { API_ROUTER } from './src/api';
+
 const app = express();
+app.use(morgan('tiny'))
 app.use('/API', API_ROUTER)
 
 app.listen(process.env.PORT)
