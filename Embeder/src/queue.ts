4f4eeb505a97547c6d437164e412d39ec539572a
Guuzzeji <5lh8dl440f@gmail.com>
2024-05-04 11:16:25 -0700

    Cleaning up queue to run with openai now

--- Embeder/src/queue.ts
+++ Embeder/src/queue.ts
@@ -4,15 +4,10 @@ import 'dotenv/config'
 
 import { WorkerProcess } from './WorkerProcess.ts';
 
-// TODO: Rewrite this to use express request between butcher and embeder to put job onto queue
-// ! NOTE: Bullmq has weird bug when under docker, can use Bull instead or other queue manager that uses redis
-
-log.info("Starting...", new Date().toLocaleString())
+log.info("Starting Queue...", new Date().toLocaleString())
 
 export const queue = new Queue(process.env.REDIS_QUEUE_NAME,
     `redis://${process.env.REDIS_SERVER_HOST}:${process.env.REDIS_SERVER_PORT}`);
-
-console.log(queue)
 
 // Note:
 // Job name = store id of video
@@ -20,17 +15,16 @@ console.log(queue)
 queue.process(async function (job, done) {
     log.info("Job Given - ", "ID:", job.name, "Data:", JSON.stringify(job.data))
     try {
-        let WorkerInstance = await WorkerProcess.initialize(job.data);
         log.info("Running job for ...", "ID:", job.name)
-        // await WorkerInstance.doJob()
+        let WorkerInstance = await WorkerProcess.initialize(job.data);
+        await WorkerInstance.doJob()
+
         done(null, {
             ok: true,
             id: job.name
         })
+
     } catch (err) {
         done(err)
     }
-
 })
-
-// queue.add({ data: "some data" })
