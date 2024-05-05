6457b1beec0a53a7c72f095dbdd80f7c42e812fa
Guuzzeji <5lh8dl440f@gmail.com>
2024-05-05 09:45:33 -0700

    Redoing frontend api request to connect either to localhost or same domain under proxy

--- Frontend/chat-ui/src/components/chatsdraw/uploadbutton/uploadVideoSlice.js
+++ Frontend/chat-ui/src/components/chatsdraw/uploadbutton/uploadVideoSlice.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
+const API_UPLOAD_URL = (process.env.REACT_APP_DEBUG === null || String(process.env.REACT_APP_DEBUG).toLowerCase() === 'true') ? process.env.REACT_APP_UPLOAD_API_URL : '/API/upload';
+
 export const sendVideo = createAsyncThunk("user/UploadVideo", async ({ title, videoFile }, thunkAPI) => {
     let formData = new FormData();
     formData.append("title", title);
@@ -8,7 +10,7 @@ export const sendVideo = createAsyncThunk("user/UploadVideo", async ({ title, vi
 
     // console.log("THE VIDEO,", title);
 
-    const res = await axios.post(process.env.REACT_APP_UPLOAD_API_URL + '/upload_video', formData, {
+    const res = await axios.post(API_UPLOAD_URL, formData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
