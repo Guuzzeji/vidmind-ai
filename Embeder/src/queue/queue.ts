d95294c8d4c6dcddb6b5b5c8b133ef7b5dec15ff
Guuzzeji <5lh8dl440f@gmail.com>
2024-05-14 08:42:47 -0700

    Just manually writing the queue names

--- Embeder/src/queue/queue.ts
+++ Embeder/src/queue/queue.ts
@@ -1,22 +1,29 @@
 import Queue from 'bull';
+import OpenAI from 'openai';
 import * as log from 'npmlog';
 import 'dotenv/config'
 
-import { WorkerProcess } from './WorkerProcess.ts';
+import { VideoWorker } from './VideoWorker.ts';
+import { sleep } from '../utils.ts';
 
 log.info("Starting Queue...", new Date().toLocaleString())
 
-export const queue = new Queue(process.env.REDIS_QUEUE_NAME,
+const OPENAI = new OpenAI();
+
+export const VIDEO_PROCESSING_QUEUE = new Queue("video-processing-ticket-pool",
+    `redis://${process.env.REDIS_SERVER_HOST}:${process.env.REDIS_SERVER_PORT}`);
+
+export const OPENAI_BATCH_CHECKING_QUEUE = new Queue("openai-batch-status-check-pool",
     `redis://${process.env.REDIS_SERVER_HOST}:${process.env.REDIS_SERVER_PORT}`);
 
 // Note:
 // Job name = store id of video
 // Job data = data of job
-queue.process(async function (job, done) {
-    log.info("Job Given - ", "ID:", job.id, "Data:", JSON.stringify(job.data))
+VIDEO_PROCESSING_QUEUE.process(async function (job, done) {
+    log.info("Job Given Video Procecssing - ", "ID:", job.id, "Data:", JSON.stringify(job.data))
     try {
         log.info("Running job for ...", "ID:", job.id)
-        let WorkerInstance = await WorkerProcess.initialize(job.data);
+        let WorkerInstance = new VideoWorker(job.data);
 
         // Use for just debug queue without running OpenAI job
         if (String(process.env.SHOULD_RUN_JOB).toLocaleLowerCase() == "true") {
@@ -31,4 +38,33 @@ queue.process(async function (job, done) {
     } catch (err) {
         done(err)
     }
+})
+
+OPENAI_BATCH_CHECKING_QUEUE.process(async function (job, done) {
+    log.info("Job Given Batch Checkup - ", "ID:", job.id, "Data:", JSON.stringify(job.data))
+    const batch = await OPENAI.batches.retrieve(job.data.batchId);
+
+    if (batch.status == "completed") {
+
+
+
+        done(null, {
+            ok: true,
+            id: job.id
+        })
+
+    } else if (batch.status == "failed") {
+        log.info("Batch Checkup FAILED - ", "ID:", job.id, "Video ID:", batch.input_file_id)
+        done(new Error(`BATCH FAILED ${batch.input_file_id} has failed`))
+
+    } else {
+        done(null, {
+            ok: true,
+            id: job.id
+        })
+
+        OPENAI_BATCH_CHECKING_QUEUE.add(job.data)
+    }
+
+    await sleep(1000 * 60)
 })
