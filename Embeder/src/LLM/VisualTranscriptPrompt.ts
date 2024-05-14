4697b3c7709beb29fa46c9196feff19ab1c29bb1
Guuzzeji <5lh8dl440f@gmail.com>
2024-05-14 08:44:30 -0700

    Modify prompt to handle single images

--- Embeder/src/LLM/VisualTranscriptPrompt.ts
+++ Embeder/src/LLM/VisualTranscriptPrompt.ts
@@ -10,11 +10,11 @@ type Image = {
 
 const AI_MODEL = "gpt-4o";
 const MAX_TOKENS = 10000;
-const SYSTEM_PROMPT = `As a Image-to-Text Conversion Specialist, your role is to provide detailed text summaries for each frame of the video. Begin by establishing context using the video title, audio transcription, and any additional text information provided to help you describe each image. Analyze each image, focusing on key elements such as actions, charts, diagrams, and text. If any image is unclear, use context from the audio transcript, other text information, and the any other images given to you to make informed assumptions. Craft a detailed summary for each frame, prioritizing the transcription of text or code and descriptions of diagrams or charts. 
+const SYSTEM_PROMPT = `As a Image-to-Text Conversion Specialist, your role is to provide detailed text summaries for a image given to you. Begin by establishing context using the title of the video, and any additional text information provided to help you describe the image. Analyze the image, focusing on key elements such as actions, charts, diagrams, and text. If any image is unclear, use the text context given to you to make a informed assumptions. Craft a detailed summary for the image, prioritizing the transcription of text or code and descriptions of diagrams or charts. 
 
 Exclude the video title and "Visual Summary:" from your response. Always include the time code at the start of your response. Also make sure to start a newline for each image you are summarizing, Do not add extra spacing between image summary, Your response should look like this:
 
-"start time code" to "end time code" - "your first response"
+"start time code" to "end time code" - "your response"
 
 Keep your response concise. If unable to summarize a frame, state: 'None'.`;
 
@@ -33,15 +33,15 @@ export class VisualTranscriptPrompt {
             { "role": "system", "content": SYSTEM_PROMPT },
             {
                 "role": "user",
-                "content": `Video Title: "${this.title}"\n`
+                "content": `Title of Image: "${this.title}"\n`
             },
             {
                 "role": "user",
-                "content": `Audio Transcription For All Images: "${audioTranscription}"\n`
+                "content": `Background Context of Image: "${audioTranscription}"\n`
             },
             {
                 "role": "user",
-                "content": `Video Screenshots Below\n`
+                "content": `Image to Summaries\n`
             },
             ...imagePrompt
         ];
