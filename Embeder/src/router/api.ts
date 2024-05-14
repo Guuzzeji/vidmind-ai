28d2851293027c3994be622508c18c1590fa909a
Guuzzeji <5lh8dl440f@gmail.com>
2024-05-14 08:43:26 -0700

    Updating request to use new queue name

--- Embeder/src/router/api.ts
+++ Embeder/src/router/api.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
-import { queue } from '../queue/queue';
+import { VIDEO_PROCESSING_QUEUE } from '../queue/queue';
 
 export const API_ROUTER = express.Router();
 API_ROUTER.use(bodyParser.json())
 
 API_ROUTER.post('/add-to-queue', function (req, res) {
     // console.log(req.body);
-
-    queue.add(req.body);
+    VIDEO_PROCESSING_QUEUE.add(req.body);
 
     res.send({
         ok: true,
